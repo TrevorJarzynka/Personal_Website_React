333a9dffbf0e77e085e3a361d4e7bdd5f64f9051
Trevor Jarzynka <tdj@Trevors-MacBook-Pro.local>
2025-07-14 21:08:22 -0400

    added quantum paper link

--- my-website/src/Projects.js
+++ my-website/src/Projects.js
@@ -1,29 +1,33 @@
 import React, { useState } from 'react';
 import './Projects.css';
+import quantumPdf from './assets/Quantum_Computing_Paper.pdf';
+import aiStudyImage from './assets/AI_Study_Assistant_Image.jpg';
+import imageClassificationImage from './assets/Image_Classification.jpg';
+import fileSystemOrganizerImage from './assets/File_system_organizer_image.jpg';
+import quantumComputerImage from './assets/Quantum_Computer_Image.jpeg';
 
 const projects = [
   {
     name: 'AI Study Assistant',
-    image: './AI_Study_Assistant_Image.jpg',
+    image: aiStudyImage,  
     description: 'Currently developing a study assistant that features natural language processing for personalized learning assistance.',
   },
   {
     name: 'Image Classification',
-    image: './Image_Classification.jpg',
+    image: imageClassificationImage,
     description: 'Designed and implemented a Convolutional Neural Network (CNN) model for image classification',
   },
   {
     name: 'File System Organizer',
-    image: './File_system_organizer_image.jpg',
+    image: fileSystemOrganizerImage,
     description: 'Developed a Python-based personalized file system organizer that automatically sorts and categorizes files by keywords, efficiently organizing them into the appropriate directories.',
   },
   {
     name: 'Quantum Computing Paper',
-    image: './Quantum_Computer_Image.jpeg',
-    description: 'A focused analysis capturing my understanding BCQ and QHE encryption techniques. These are two essential protocols for enabling secure quantum computing.'
+    image: quantumComputerImage,
+    description: 'A focused analysis capturing my understanding BCQ and QHE encryption techniques. These are two essential protocols for enabling secure quantum computing.',
+    link: quantumPdf,
   }
- 
-  
 ];
 
 function Projects() {
@@ -42,7 +46,15 @@ function Projects() {
             onMouseLeave={() => setHoveredProject(null)}
           >
             <div className={`project-content ${hoveredProject === index ? 'expanded' : ''}`}>
-              <h3 className="project-name">{project.name}</h3>
+              <h3 className="project-name">
+                {project.link ? (
+                  <a href={project.link} target="_blank" rel="noopener noreferrer">
+                    {project.name}
+                  </a>
+                ) : (
+                  project.name
+                )}
+              </h3>
               <p className="project-description">{project.description}</p>
             </div>
           </div>
